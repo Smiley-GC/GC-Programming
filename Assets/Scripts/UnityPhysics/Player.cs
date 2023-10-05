984b0390a50455708e11c5d29439e994760546b7
Smiley98 <smileysofficialcinema@gmail.com>
2023-10-05 13:09:22 -0400

    Added unity physics

---
+++ Assets/Scripts/UnityPhysics/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0.0f, speed);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.velocity = new Vector2(0.0f, -speed);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, 0.0f);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, 0.0f);
+        }
+
+        else
+        {
+            rb.velocity = Vector2.zero;
+            // Equivalent to below:
+            // new Vector2(0.0f, 0.0f);
+        }
+    }
+}
