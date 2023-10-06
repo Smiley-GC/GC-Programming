a5fe47a0cce989db118bd5ed06c67bf96f4479ba
Smiley98 <smileysofficialcinema@gmail.com>
2023-10-06 13:22:04 -0400

    Added physics-based movement

--- Assets/Scripts/UnityPhysics/Player.cs
+++ Assets/Scripts/UnityPhysics/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 0.0f;
     Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -15,29 +16,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xDir = 0.0f;
+        float yDir = 0.0f;
         if (Input.GetKey(KeyCode.W))
         {
-
+            yDir = 1.0f;
         }
 
         else if (Input.GetKey(KeyCode.S))
         {
-
+            yDir = -1.0f;
         }
 
         else if (Input.GetKey(KeyCode.A))
         {
-
+            xDir = -1.0f;
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-
+            xDir = 1.0f;
         }
 
-        else
-        {
-            rb.velocity = Vector3.zero;
-        }
+        rb.velocity = new Vector2(xDir, yDir) * speed;
     }
 }
