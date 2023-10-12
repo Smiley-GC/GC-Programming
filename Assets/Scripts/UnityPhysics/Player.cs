711b5237d192019539ef37ed0492eca1b37edc3c
Smiley98 <smileysofficialcinema@gmail.com>
2023-10-12 14:28:49 -0400

    Added homework 5 task.

--- Assets/Scripts/UnityPhysics/Player.cs
+++ Assets/Scripts/UnityPhysics/Player.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Homework - modify the code so that:
+// Rotation speed doubles when it enters the SpeedRectangle.
+// Rotation speed should halve when you enter the SlowCircle.
+// Rotation speed should reset to its initial value after exiting either shape.
 public class Player : MonoBehaviour
 {
     public float speed = 0.0f;
@@ -38,24 +42,34 @@ public class Player : MonoBehaviour
         Vector3 forward = transform.right;
         Vector3 right = transform.up;
 
+        // Must convert to vec2 if you'd like to apply these to Rigidbody2D
+        //Vector2 forward = transform.right;
+        //Vector2 right = transform.up;
+
         // 2. Move along direction
+        //rb.velocity = Vector2.zero;
+
         if (Input.GetKey(KeyCode.W))
         {
+            //rb.velocity += forward * speed;
             transform.position += forward * speed * dt;
         }
         
         else if (Input.GetKey(KeyCode.S))
         {
+            //rb.velocity -= forward * speed;
             transform.position -= forward * speed * dt;
         }
         
         if (Input.GetKey(KeyCode.A))
         {
+            //rb.velocity += right * speed;
             transform.position += right * speed * dt;
         }
         
         else if (Input.GetKey(KeyCode.D))
         {
+            //rb.velocity -= right * speed;
             transform.position -= right * speed * dt;
         }
 
