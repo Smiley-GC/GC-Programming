68edec325595f1b56935b7b56896bb84c2de64a0
Smiley98 <smileysofficialcinema@gmail.com>
2023-10-13 13:02:06 -0400

    Added rotation challenge

--- Assets/Scripts/UnityPhysics/Player.cs
+++ Assets/Scripts/UnityPhysics/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 0.0f;
+    public float rotationSpeed = 0.0f;
     Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -16,28 +17,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xDir = 0.0f;
-        float yDir = 0.0f;
-        if (Input.GetKey(KeyCode.W))
-        {
-            yDir = 1.0f;
-        }
+        float dt = Time.deltaTime;
+        //transform.rotation = transform.rotation * Quaternion.Euler(0.0f, 0.0f, rotationSpeed * dt);
+        // Challenge: Rotate left when Q is down, rotate right when E is down.
 
-        else if (Input.GetKey(KeyCode.S))
-        {
-            yDir = -1.0f;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            xDir = -1.0f;
-        }
-
-        else if (Input.GetKey(KeyCode.D))
-        {
-            xDir = 1.0f;
-        }
-
-        rb.velocity = new Vector2(xDir, yDir).normalized * speed;
+        //float xDir = 0.0f;
+        //float yDir = 0.0f;
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    yDir = 1.0f;
+        //}
+        //
+        //else if (Input.GetKey(KeyCode.S))
+        //{
+        //    yDir = -1.0f;
+        //}
+        //
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    xDir = -1.0f;
+        //}
+        //
+        //else if (Input.GetKey(KeyCode.D))
+        //{
+        //    xDir = 1.0f;
+        //}
+        //
+        //rb.velocity = new Vector2(xDir, yDir).normalized * speed;
     }
 }
