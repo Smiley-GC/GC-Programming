c673e9ffcf3aa57e2ff71aa0ff5c05473007bee2
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-02 12:44:01 -0400

    Added functions scene & script

---
+++ Assets/Scripts/Functions/Functions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    void Test()
+    {
+        Debug.Log("Testing... 1, 2, 3!");
+    }
+
+    void Start()
+    {
+        Test();
+    }
+
+    void Update()
+    {
+        
+    }
+}
