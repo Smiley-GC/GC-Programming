86f592a205113935c8dbfbea6584b7748cdbd33b
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-03 12:52:54 -0400

    Added functions script

---
+++ Assets/Scripts/Functions/Functions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    void Test()
+    {
+        Debug.Log("Testing... 1, 2, 3!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
