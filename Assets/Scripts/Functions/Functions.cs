1f96753fd4c35be2afe0c8036e09451ce49429e7
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-03 13:22:03 -0400

    Added MoveObject function to allow for custom directional movement

--- Assets/Scripts/Functions/Functions.cs
+++ Assets/Scripts/Functions/Functions.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Functions : MonoBehaviour
 {
+    float speed = 10.0f;
+
     void MovePlayer(float speed)
     {
         transform.position += Vector3.forward * speed * Time.deltaTime;
     }
 
+    Vector3 MoveObject(Vector3 direction, float speed)
+    {
+        return direction * speed * Time.deltaTime;
+    }
+
     void Update()
     {
-        MovePlayer(10.0f);
+        //MovePlayer(10.0f);
+        transform.position += MoveObject(transform.right, speed);
     }
 }
