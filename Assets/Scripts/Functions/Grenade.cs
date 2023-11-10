b0ad86954c27e5f91faa0bd7211af25b195f72a5
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-10 13:31:09 -0500

    Added grenade destruction

--- Assets/Scripts/Functions/Grenade.cs
+++ Assets/Scripts/Functions/Grenade.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // TODO -- make an actual explosion xD
+    // https://learn.unity.com/tutorial/introduction-to-particle-systems
+    // https://docs.unity3d.com/Manual/ParticleSystems.html
+    void OnDestroy()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("BOOM");
     }
 }
