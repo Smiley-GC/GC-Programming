f3f280e8ef21a491bb2acbbd33a1383df5c51434
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-10 13:16:39 -0500

    Added for loop

--- Assets/Scripts/Loops/Loops.cs
+++ Assets/Scripts/Loops/Loops.cs
@@ -6,11 +6,26 @@ public class Loops : MonoBehaviour
 {
     void Start()
     {
+        //TestWhileLoop();
+        TestForLoop();
+    }
+
+    void TestWhileLoop()
+    {
         int counter = 1;
         while (counter <= 5)
         {
             Debug.Log("Looping x" + counter);
             counter++;
+        }
+        Debug.Log("No longer looping");
+    }
+    
+    void TestForLoop()
+    {
+        for (int counter = 1; counter <= 5; counter++)
+        {
+            Debug.Log("Looping x" + counter);
         }
         Debug.Log("No longer looping");
     }
