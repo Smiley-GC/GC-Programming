162b8e81abdf1ba58261d83555ad75ec3cbb4c20
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-17 13:21:11 -0500

    Reassigned values in array

--- Assets/Scripts/Collections/Collections.cs
+++ Assets/Scripts/Collections/Collections.cs
@@ -13,21 +13,44 @@ public class Collections : MonoBehaviour
     {
         // "Out of bounds" -- be sure you only access elements that exist!
         //Debug.Log("Value at index 5: " + automaticArray[5]);
-        //TestArrayAccess();
+        TestArrayAccess();
     }
 
     void TestArrayAccess()
     {
         // Manual access
-        Debug.Log("Value at index 0: " + automaticArray[0]);
-        Debug.Log("Value at index 1: " + automaticArray[1]);
-        Debug.Log("Value at index 2: " + automaticArray[2]);
-        Debug.Log("Value at index 3: " + automaticArray[3]);
-        Debug.Log("Value at index 4: " + automaticArray[4]);
+        //Debug.Log("Value at index 0: " + automaticArray[0]);
+        //Debug.Log("Value at index 1: " + automaticArray[1]);
+        //Debug.Log("Value at index 2: " + automaticArray[2]);
+        //Debug.Log("Value at index 3: " + automaticArray[3]);
+        //Debug.Log("Value at index 4: " + automaticArray[4]);
 
         // Automatic access
         for (int i = 0; i < automaticArray.Length; i++)
+        {
             Debug.Log(automaticArray[i]);
+        }
+
+        // Change all values to the same value
+        for (int i = 0; i < automaticArray.Length; i++)
+        {
+            automaticArray[i] = 420;
+            Debug.Log(automaticArray[i]);
+        }
+
+        // Make our values ascending
+        for (int i = 0; i < automaticArray.Length; i++)
+        {
+            automaticArray[i] = i + 1;
+            Debug.Log(automaticArray[i]);
+        }
+
+        // Make our values descending
+        for (int i = 0; i < automaticArray.Length; i++)
+        {
+            automaticArray[i] = automaticArray.Length - i;
+            Debug.Log(automaticArray[i]);
+        }
     }
 
     // Update is called once per frame
