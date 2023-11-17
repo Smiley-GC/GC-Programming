d1199f9f2fa246611897817cd7b8d17ee4ed1947
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-17 12:54:10 -0500

    Manually accessed each element

--- Assets/Scripts/Collections/Collections.cs
+++ Assets/Scripts/Collections/Collections.cs
@@ -10,7 +10,14 @@ public class Collections : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Value at index 0: " + automaticArray[0]);
+        Debug.Log("Value at index 1: " + automaticArray[1]);
+        Debug.Log("Value at index 2: " + automaticArray[2]);
+        Debug.Log("Value at index 3: " + automaticArray[3]);
+        Debug.Log("Value at index 4: " + automaticArray[4]);
+
+        // "Out of bounds" -- be sure you only access elements that exist!
+        //Debug.Log("Value at index 5: " + automaticArray[5]);
     }
 
     // Update is called once per frame
