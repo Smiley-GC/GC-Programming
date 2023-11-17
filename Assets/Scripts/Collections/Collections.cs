a893ff1a26b1a477857fc761fb4ca92598fd336c
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-17 12:45:29 -0500

    Added collections scene

---
+++ Assets/Scripts/Collections/Collections.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collections : MonoBehaviour
+{
+    public int[] manualArray = new int[20];
+    public int[] automaticArray = { 1, 2, 3, 4, 5 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
