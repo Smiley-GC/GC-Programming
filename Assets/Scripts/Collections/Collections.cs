5c586fbac4dd7794112cdde3d01a23f7cc421fca
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-17 12:57:55 -0500

    Added automatic access

--- Assets/Scripts/Collections/Collections.cs
+++ Assets/Scripts/Collections/Collections.cs
@@ -10,14 +10,19 @@ public class Collections : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // "Out of bounds" -- be sure you only access elements that exist!
+        //Debug.Log("Value at index 5: " + automaticArray[5]);
+
+        // Manual access
         Debug.Log("Value at index 0: " + automaticArray[0]);
         Debug.Log("Value at index 1: " + automaticArray[1]);
         Debug.Log("Value at index 2: " + automaticArray[2]);
         Debug.Log("Value at index 3: " + automaticArray[3]);
         Debug.Log("Value at index 4: " + automaticArray[4]);
 
-        // "Out of bounds" -- be sure you only access elements that exist!
-        //Debug.Log("Value at index 5: " + automaticArray[5]);
+        // Automatic access
+        for (int i = 0;  i < automaticArray.Length; i++)
+            Debug.Log(automaticArray[i]);
     }
 
     // Update is called once per frame
