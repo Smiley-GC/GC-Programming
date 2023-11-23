b08d9c41ff3264421942c292572bcf53c9ca5d79
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-23 09:03:32 -0500

    Added homework 7 solution and added detection to homework 8 resolution function

--- Assets/Scripts/Math/CircleCircleDetection.cs
+++ Assets/Scripts/Math/CircleCircleDetection.cs
@@ -7,48 +7,34 @@ public class CircleCircleDetection : MonoBehaviour
     public GameObject circle1;
     public GameObject circle2;
 
-    // Homework: create a function to detect if the two circles are overlapping
-    // Recommendation: the function should take in two positions, and two radiuses
-    // The function should check if the distance between centres is less than or
-    // equal to the sum of the radii. If so, colour the circles green, otherwise red.
-
-    // Homework 7 hint:
-    bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2)
-    {
-        // 1. Calculate distance between position1 and position2
-        float distance = Vector2.Distance(position1, position2);
-
-        // 2. Calculate sum of radius1 + radius2
-        float radiiSum = radius1 + radius2;
-
-        // 3. Compare whether the distance is less than the radii sum (if so, there's a collision)!
-        return distance < radiiSum;
-    }
-
     // Homework 8: Upgrade our CheckCollisionCircles function to resolve collision between two circles by applying the mtv.
     // Calculate the mtv's direction by determining the direction from 2 to 1 (subtract 1 from 2 then normalize the result)
     // Calculate the mtv's magnitude by determining penetration depth. Subtract radii sum by distance between centres.
     // Only write to the mtv if there's a collision!
-    //bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2, out Vector2 mtv)
-    //{
-    //    // Shortcut for distance (between positions 1 & 2):
-    //    float distance = Vector2.Distance(position1, position2);
-    //
-    //    // Shortcut for direction from 1 to 2 (normalization):
-    //    Vector2 direction = (position1 - position2).normalized;
-    //
-    //    bool collision = false;
-    //    if (collision)
-    //    {
-    //        // Calculate mtv only if there's a collision
-    //        mtv = Vector2.zero; // replace this with actual calculations
-    //    }
-    //    else
-    //    {
-    //        mtv = Vector2.zero;
-    //    }
-    //    return collision;
-    //}
+    bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2, out Vector2 mtv)
+    {
+        // Distance between position 1 and position 2
+        float distance = Vector2.Distance(position1, position2);
+
+        // Direction from to position 2 to position 1
+        Vector2 direction = (position1 - position2).normalized;
+
+        // Sum of radii
+        float radiiSum = radius1 + radius2;
+
+        // Collision if distance between circles is less than the sum of their radii!
+        bool collision = distance < radiiSum;
+        if (collision)
+        {
+            // Calculate mtv only if there's a collision
+            mtv = Vector2.zero; // replace this with actual calculations
+        }
+        else
+        {
+            mtv = Vector2.zero;
+        }
+        return collision;
+    }
 
     void Update()
     {
@@ -56,9 +42,11 @@ public class CircleCircleDetection : MonoBehaviour
         Vector2 position2 = circle2.transform.position;
         float radius1 = circle1.transform.localScale.x * 0.5f;
         float radius2 = circle2.transform.localScale.x * 0.5f;
-        //Vector2 mtv = Vector2.zero;
-        //bool collision = CheckCollisionCircles(position1, radius1, position2, radius2, out mtv);
-        bool collision = CheckCollisionCircles(position1, radius1, position2, radius2);
+
+        // MTV resolves 1 from 2
+        Vector2 mtv = Vector2.zero;
+        bool collision = CheckCollisionCircles(position1, radius1, position2, radius2, out mtv);
+        circle1.transform.position += new Vector3(mtv.x, mtv.y, 0.0f);
         Color color = collision ? Color.green : Color.red;
 
         // Vector2.Distance automatically calculates the distance between two points
@@ -85,4 +73,19 @@ public class CircleCircleDetection : MonoBehaviour
         //    color = Color.red;
         //}
     }
+
+    // Homework 7 solution
+    /*
+    bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2)
+    {
+        // 1. Calculate distance between position1 and position2
+        float distance = Vector2.Distance(position1, position2);
+
+        // 2. Calculate sum of radius1 + radius2
+        float radiiSum = radius1 + radius2;
+
+        // 3. Compare whether the distance is less than the radii sum (if so, there's a collision)!
+        return distance < radiiSum;
+    }
+    */
 }
