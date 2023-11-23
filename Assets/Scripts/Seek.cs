8182e486a1e173b60f7881909d8621cab4b2d1a0
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-23 13:53:05 -0500

    Added bullet list add & remove

--- Assets/Scripts/Seek.cs
+++ Assets/Scripts/Seek.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Homework 9: Destroy all bullets and remove them from the bullets list if ANY bullets hit the target (Player).
 public class Seek : MonoBehaviour
 {
-    // Homework task 1: if the AI has detected the player, shoot at it (1%)!
     public GameObject target;
     public GameObject bullet;
+    List<GameObject> bullets = new List<GameObject>();
 
     float movementSpeed = 2.0f;
     float bulletSpeed = 10.0f;
@@ -39,6 +40,7 @@ public class Seek : MonoBehaviour
         Vector3 bulletPosition = transform.position + bulletDirection;
         GameObject bulletClone = Instantiate(bullet, bulletPosition, Quaternion.identity);
         bulletClone.GetComponent<Rigidbody2D>().velocity = bulletDirection * bulletSpeed;
+        bullets.Add(bulletClone);
     }
 
     void Update()
@@ -56,6 +58,17 @@ public class Seek : MonoBehaviour
                 ShootBullet();
             }
             elapsed += dt;
+        }
+
+        // Delete bullets if they're off-screen, and/or have been alive for more than 5 seconds.
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            Bullet bulletData = bullets[i].GetComponent<Bullet>();
+            if (bulletData.age > 1.0f)
+            {
+                Destroy(bulletData.gameObject);
+                bullets.Remove(bulletData.gameObject);
+            }
         }
     }
 }
