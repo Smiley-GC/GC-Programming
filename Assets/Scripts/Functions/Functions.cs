806b2c3dfa04f16eb54c6483a7639a0e03e19a18
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-23 12:33:52 -0500

    Added homework 6 solution

--- Assets/Scripts/Functions/Functions.cs
+++ Assets/Scripts/Functions/Functions.cs
@@ -14,7 +14,31 @@ using UnityEngine;
 // A function that spawns and/or moves objects is considered "awesome" and will give full marks.
 public class Functions : MonoBehaviour
 {
+    // These basic arithmetic functions aren't useful since C# has + - * / built into the language.
+    // However, if we need to apply constraints to our functions (ie limiting values to be between 0 & 5) then they are useful!
     float speed = 10.0f;
+
+    float Add(float a, float b)
+    {
+        a = Mathf.Clamp(a, 0.0f, 5.0f);
+        b = Mathf.Clamp(b, 0.0f, 5.0f);
+        return a + b;
+    }
+
+    float Sub(float a, float b)
+    {
+        return a - b;
+    }
+
+    float Mul(float a, float b)
+    {
+        return a * b;
+    }
+
+    float Div(float a, float b)
+    {
+        return a / b;
+    }
 
     void MovePlayer(float speed)
     {
@@ -24,6 +48,22 @@ public class Functions : MonoBehaviour
     Vector3 MoveObject(Vector3 direction, float speed)
     {
         return direction * speed * Time.deltaTime;
+    }
+
+    void Start()
+    {
+        float x = 10.0f;
+        float y = 10.0f;
+
+        Debug.Log(x + y);
+        Debug.Log(x - y);
+        Debug.Log(x * y);
+        Debug.Log(x / y);
+
+        Debug.Log(Add(x, y));
+        Debug.Log(Sub(x, y));
+        Debug.Log(Mul(x, y));
+        Debug.Log(Div(x, y));
     }
 
     void Update()
