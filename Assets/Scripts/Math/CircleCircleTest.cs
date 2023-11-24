324b77db3bc8e72907e1cdb2615eeccefd9a3b3f
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-24 09:12:23 -0500

    Added homework 8 hint

--- Assets/Scripts/Math/CircleCircleTest.cs
+++ Assets/Scripts/Math/CircleCircleTest.cs
@@ -12,59 +12,61 @@ public class CircleCircleTest : MonoBehaviour
         //VectorReview();
     }
 
-    // Homework 7: write a function to determine if two circles are overlapping
-    // Recommendation: keep your functions pure math -- take in 2 positions and 2 radii as arguments
-    // return the result of the collision as a boolean
-    // Homework 7 hint:
-    bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2)
-    {
-        return Vector2.Distance(position1, position2) < radius1 + radius2;
-    }
-
     // Homework 8: Upgrade the previous function so that it calculates the MTV ("minimum translation vector").
     // If the two circles are overlapping, apply the mtv to circle 1 to resolve the collision!
     // Remember, mtv resolves 1 FROM 2
-    //bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2,
-    //    out Vector2 mtv)
-    //{
-    //    // Distance between position1 and position2
-    //    float distance = Vector2.Distance(position1, position2);
-    //
-    //    // Unit vector pointing FROM 2 TO 1
-    //    Vector2 direction = (position1 - position2).normalized;
-    //
-    //    bool collision = false; // fix this to be a comparison between distance and radii sum
-    //    if (collision)
-    //    {
-    //        // Change this to be direction * depth
-    //        mtv = Vector2.zero;
-    //    }
-    //    else
-    //    {
-    //        mtv = Vector2.zero;
-    //    }
-    //    return collision;
-    //}
+    bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2, out Vector2 mtv)
+    {
+        // Distance between position1 and position2
+        float distance = Vector2.Distance(position1, position2);
+
+        // Unit vector pointing FROM 2 TO 1
+        // (AB = B - A. Its up to you which is A and which is B)
+        Vector2 direction = (position1 - position2).normalized;
+
+        float radiiSum = radius1 + radius2;
+        bool collision = distance < radiiSum;
+        if (collision)
+        {
+            // mtv = direction * depth
+            // depth = radiiSum - distance
+            // We already have direction
+            mtv = Vector2.zero; // (replace with actual mtv formula)
+        }
+        else
+        {
+            mtv = Vector2.zero;
+        }
+        return collision;
+    }
 
     void Update()
     {
-        //Vector2 mtv = Vector2.zero;
         Vector2 position1 = circle1.transform.position;
         Vector2 position2 = circle2.transform.position;
         float radius1 = circle1.transform.localScale.x * 0.5f;
         float radius2 = circle2.transform.localScale.x * 0.5f;
-        bool collision = CheckCollisionCircles(position1, radius1, position2, radius2);
-        if (collision)
-        {
-            circle1.GetComponent<SpriteRenderer>().color = Color.green;
-            circle2.GetComponent<SpriteRenderer>().color = Color.green;
-            //circle1.transform.position += new Vector3(mtv.x, mtv.y, 0.0f);
-        }
-        else
-        {
-            circle1.GetComponent<SpriteRenderer>().color = Color.red;
-            circle2.GetComponent<SpriteRenderer>().color = Color.red;
-        }
+
+        // MTV resolves 1 from 2 (because it points from 2 to 1)
+        Vector2 mtv = Vector2.zero;
+        bool collision = CheckCollisionCircles(position1, radius1, position2, radius2, out mtv);
+        circle1.transform.position += new Vector3(mtv.x, mtv.y, 0.0f);
+
+        Color color = collision ? Color.green : Color.red;
+        circle1.GetComponent<SpriteRenderer>().color = color;
+        circle2.GetComponent<SpriteRenderer>().color = color;
+
+        // "ternary" operator (the question mark) assigned based on a true/false value.
+        // It assigns to the left of the : if true, otherwise it assigns to the right of the : if false.
+        // Its equivalent to the following code:
+        //if (collision)
+        //{
+        //    color = Color.green;
+        //}
+        //else
+        //{
+        //    color = Color.red;
+        //}
     }
 
     void VectorReview()
