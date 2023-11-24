eec019fa468b12fe357404a7fba99745a92cee99
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-24 14:16:49 -0500

    Homework 8 hint

--- Assets/Scripts/Collections/Collections.cs
+++ Assets/Scripts/Collections/Collections.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Collections : MonoBehaviour
 {
+    // Option 1: Populate an array of game objects using the inspector
+    public GameObject[] gameObjects1;
+
+    // Option 2: Make space for 3 game objects and instantiate them using a prefab
+    GameObject[] gameObjects2 = new GameObject[3];
+    public GameObject prefab;
+
+    void CreateGameObjects()
+    {
+        // Option 1 doesn't require us to instantiate game objects with code because we dragged them into the inspector
+        // Option 2 requires us to use the Instantiate function:
+        for (int i = 0; i < gameObjects2.Length; i++)
+        {
+            gameObjects2[i] = Instantiate(prefab);
+            gameObjects2[i].transform.position = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), 0.0f);
+        }
+    }
+
     public int[] manualArray = new int[20];
     public int[] automaticArray = { 1, 2, 3, 4, 5 };
     int index = 0;
@@ -11,10 +29,14 @@ public class Collections : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        CreateGameObjects();
+
         // "Out of bounds" -- be sure you only access elements that exist!
         //Debug.Log("Value at index 5: " + automaticArray[5]);
         TestArrayAccess();
     }
+
+    
 
     void TestArrayAccess()
     {
@@ -53,9 +75,10 @@ public class Collections : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // TODO -- loop through MoveObject here for homework 8
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log(automaticArray[index]);
