ef358b6f60ef552614c0a5a4ef6b5ec1bb02530d
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-24 13:03:20 -0500

    Added homework 7 AI shooting solution

--- Assets/Scripts/AI/Seek.cs
+++ Assets/Scripts/AI/Seek.cs
@@ -6,22 +6,49 @@ using UnityEngine;
 public class Seek : MonoBehaviour
 {
     public GameObject target;
+    public GameObject bulletPrefab;
+
+    float currentTime = 0.0f;
+    float totalTime = 0.5f;
+
     float speed = 2.0f;
-    bool seeking = false;
+    bool targetDetected = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        seeking = true;
+        if (!collision.tag.Equals("Projectile"))
+        {
+            targetDetected = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        seeking = false;
+        if (!collision.tag.Equals("Projectile"))
+        {
+            targetDetected = false;
+        }
     }
 
     void Update()
     {
-        if (seeking)
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        float dt = Time.deltaTime;
+        if (targetDetected)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * dt);
+
+            // If current time is greater than total time, fire a bullet and reset the timer!
+            if (currentTime >= totalTime)
+            {
+                // AB = B - A
+                // We want to go FROM the enemy TO the player, so enemy will be A and player will be B
+                Vector3 bulletDirection = (target.transform.position - transform.position).normalized;
+                GameObject bullet = Instantiate(bulletPrefab);
+                bullet.transform.position = transform.position + bulletDirection;
+                bullet.GetComponent<Rigidbody2D>().velocity = bulletDirection * speed * 5.0f;
+                currentTime = 0.0f;
+            }
+            currentTime += dt;
+        }
     }
 }
