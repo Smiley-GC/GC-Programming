c14c0c17f0bf45fb3b85bc3151386ee8e2a51554
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-01 13:18:37 -0500

    Added boss battle

--- Assets/Scripts/Objects/Objects.cs
+++ Assets/Scripts/Objects/Objects.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
-public class Character1
+public class Character
 {
-    public float health = 100;
+    public float health = 100.0f;
     public float mana = 100.0f;
     public float armour = 1.0f;
     public float damage = 10.0f;
@@ -13,40 +14,62 @@ public class Character1
     public string name = "";
 }
 
-public class Character2
-{
-    public float health;
-    public float mana;
-    public float armour;
-    public float damage;
-    public float movementSpeed;
-    public int score;
-    public string name;
-}
-
 public class Objects : MonoBehaviour
 {
-    Character1 character1 = new Character1();
-    Character2 character2 = new Character2();
+    void PrintStats(Character c)
+    {
+        Debug.Log(c.health);
+        Debug.Log(c.mana);
+        Debug.Log(c.armour);
+        Debug.Log(c.damage);
+        Debug.Log(c.movementSpeed);
+        Debug.Log(c.score);
+        Debug.Log(c.name);
+    }
+
+    void Combat(Character player, Character ally, Character boss)
+    {
+        while (player.health > 0.0f && ally.health > 0.0f && boss.health > 0)
+        {
+            player.health -= boss.damage;
+            ally.health -= boss.damage;
+            boss.health -= player.damage;
+            boss.health -= ally.damage;
+        }
+        if (boss.health > 0)
+            Debug.Log(boss.name + " is victorious!");
+        else
+        {
+            Debug.Log(player.name + " lives another day...");
+            Debug.Log(ally.name + " lives another day...");
+        }
+    }
+
+    Character player = new Character();
+    Character ally = new Character();
+    Character boss = new Character();
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(character1.health);
-        Debug.Log(character1.mana);
-        Debug.Log(character1.armour);
-        Debug.Log(character1.damage);
-        Debug.Log(character1.movementSpeed);
-        Debug.Log(character1.score);
-        Debug.Log(character1.name);
+        player.name = "Justin";
+        player.health = 125.0f;
+        player.mana = 150.0f;
+        player.damage *= 2.0f;
 
-        Debug.Log(character2.health);
-        Debug.Log(character2.mana);
-        Debug.Log(character2.armour);
-        Debug.Log(character2.damage);
-        Debug.Log(character2.movementSpeed);
-        Debug.Log(character2.score);
-        Debug.Log(character2.name);
+        ally.name = "Andrew";
+        ally.health = 75.0f;
+        ally.mana = 100.0f;
+        ally.damage *= 2.0f;
+
+        boss.name = "Joey";
+        boss.health = 250.0f;
+        boss.mana = 175.0f;
+
+        //PrintStats(player);
+        //PrintStats(ally);
+        //PrintStats(boss);
+        Combat(player, ally, boss);
     }
 
     // Update is called once per frame
