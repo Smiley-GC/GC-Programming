46f217d1331cf2baca880b38112812f58d8c62c6
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-01 13:37:10 -0500

    Added homework 10

--- Assets/Scripts/Math/CircleCircleDetection.cs
+++ Assets/Scripts/Math/CircleCircleDetection.cs
@@ -2,28 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// *Programming* homework 10:
+// Change the CheckCollisionCircles function to take in two Circle objects.
+// Each circle should store a position and a radius.
+
+//public class Circle
+//{
+///* insert data here*/
+//}
+
 public class CircleCircleDetection : MonoBehaviour
 {
     public GameObject circle1;
     public GameObject circle2;
 
-    // Homework: create a function to detect if the two circles are overlapping
-    // Recommendation: the function should take in two positions, and two radiuses
-    // The function should check if the distance between centres is less than or
-    // equal to the sum of the radii. If so, colour the circles green, otherwise red.
-
     void Update()
     {
-        bool collision = false;
-        if (collision)
-        {
-            circle1.GetComponent<SpriteRenderer>().color = Color.green;
-            circle2.GetComponent<SpriteRenderer>().color = Color.green;
-        }
-        else
-        {
-            circle1.GetComponent<SpriteRenderer>().color = Color.red;
-            circle2.GetComponent<SpriteRenderer>().color = Color.red;
-        }
+        Vector3 position1 = circle1.transform.position;
+        Vector3 position2 = circle2.transform.position;
+        float radius1 = circle1.transform.localScale.x * 0.5f;
+        float radius2 = circle2.transform.localScale.x * 0.5f;
+
+        Color color = CheckCollisionCircles(position1, radius1, position2, radius2) ?
+            Color.green : Color.red;
+
+        // The above is a shortcut for the following:
+        //if (CheckCollisionCircles(position1, radius1, position2, radius2))
+        //    color = Color.green;
+        //else
+        //    color = Color.red;
+
+        circle1.GetComponent<SpriteRenderer>().color = color;
+        circle2.GetComponent<SpriteRenderer>().color = color;
+    }
+
+    // Updated function should look like this:
+    // bool CheckCollisionCircles(Circle circle1, Circle circle2) { ... }
+    bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2)
+    {
+        // 1. Calculate distance between position1 and position2
+        float distance = Vector2.Distance(position1, position2);
+
+        // 2. Calculate sum of radius1 + radius2
+        float radiiSum = radius1 + radius2;
+
+        // 3. Compare whether the distance is less than the radii sum (if so, there's a collision)!
+        return distance < radiiSum;
     }
 }
