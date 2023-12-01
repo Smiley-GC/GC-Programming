d9e5b32f49d9a075d534247e52a81252bd7d0026
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-01 14:16:20 -0500

    Added multiple particles

--- Assets/Scripts/Math/PhysicsSystem.cs
+++ Assets/Scripts/Math/PhysicsSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Particle
 {
@@ -12,14 +13,43 @@ public class Particle
 public class PhysicsSystem : MonoBehaviour
 {
     public GameObject prefab;
-    GameObject particleGO;
-    Particle particle = new Particle();
+
+    // Single object
+    //Particle particle = new Particle();
+    //GameObject particleGO;
+    
+    // Multiple objects
+    List<Particle> particles = new List<Particle>();    // Physics
+    List<GameObject> objects = new List<GameObject>();  // Rendering
+
+    void Add(Particle particle)
+    {
+        particles.Add(particle);
+        objects.Add(Instantiate(prefab));
+    }
+
+    private void ResetParticles()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Particle particle = particles[i];
+            particle.pos = Vector2.zero;
+            particle.vel = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+            particle.acc = Physics.gravity;
+        }
+    }
 
     void Start()
     {
-        particleGO = Instantiate(prefab);
-        particle.vel = new Vector2(0.0f, 10.0f);
-        particle.acc = Physics.gravity;
+        // Single object
+        //particleGO = Instantiate(prefab);
+        //particle.vel = new Vector2(0.0f, 10.0f);
+        //particle.acc = Physics.gravity;
+
+        // Multiple objects
+        for (int i = 0; i < 10; i++)
+            Add(new Particle());
+        ResetParticles();
     }
 
     // pi = 5m, v = 2.5ms, t = 2s
@@ -34,13 +64,26 @@ public class PhysicsSystem : MonoBehaviour
     {
         float dt = Time.fixedDeltaTime;
 
-        // 1. Add acceleration * time to velocity
-        particle.vel = Integrate(particle.vel, particle.acc, dt);
+        for (int i = 0; i < particles.Count; i++)
+        {
+            Particle particle = particles[i];
 
-        // 2. Add velocity * time to position
-        particle.pos = Integrate(particle.pos, particle.vel, dt);
+            // 1. Add acceleration * time to velocity
+            particle.vel = Integrate(particle.vel, particle.acc, dt);
 
-        // 3. Write position from particle to game object
-        particleGO.transform.position = particle.pos;
+            // 2. Add velocity * time to position
+            particle.pos = Integrate(particle.pos, particle.vel, dt);
+
+            // 3. Write position from particle to game object
+            objects[i].transform.position = particle.pos;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ResetParticles();
+        }
     }
 }
