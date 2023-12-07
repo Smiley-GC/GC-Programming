a589cc0fc5b73f43dc4f37f3e4257fbc8135c9e6
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-07 14:07:15 -0500

    Added homework 10 hint

--- Assets/Scripts/Math/Detection.cs
+++ Assets/Scripts/Math/Detection.cs
@@ -2,32 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// *Programming* homework 10:
-// Change the CheckCollisionCircles function to take in two Circle objects.
-// Each circle should store a position and a radius.
+struct Circle
+{
+    public Vector2 position;
+    public float radius;
+}
 
+struct Rectangle
+{
+    public Vector2 position;
+    public Vector2 extents;
+}
+
+// Extra practice: add the mtv to CheckCollisionCircleRect.
+// Calculate the mtv by forming a line FROM the rectangle TO the circle,
+// then multiply it by the collision depth which is radius - distance (same as circle-circle mtv).
 public class Detection : MonoBehaviour
 {
-    public GameObject rect1;
-    public GameObject circle1;
-    public GameObject circle2;
+    public GameObject rect1GO;
+    public GameObject circle1GO;
+    public GameObject circle2GO;
+
+    Rectangle rect1 = new Rectangle();
+    Circle circle1 = new Circle();
 
     void Update()
     {
-        Vector3 rectPosition = rect1.transform.position;
-        Vector3 position1 = circle1.transform.position;
-        Vector3 position2 = circle2.transform.position;
-        float radius1 = circle1.transform.localScale.x * 0.5f;
-        float radius2 = circle2.transform.localScale.x * 0.5f;
-        Vector2 rectExtents = new Vector2(rect1.transform.localScale.x, rect1.transform.localScale.y) * 0.5f;
+        //Vector3 rectPosition = rect1GO.transform.position;
+        //Vector3 position1 = circle1GO.transform.position;
+        //Vector3 position2 = circle2GO.transform.position;
+        //float radius1 = circle1GO.transform.localScale.x * 0.5f;
+        //float radius2 = circle2GO.transform.localScale.x * 0.5f;
+        //Vector2 rectExtents = new Vector2(rect1GO.transform.localScale.x, rect1GO.transform.localScale.y) * 0.5f;
+
+        circle1.position = circle1GO.transform.position;
+        circle1.radius = circle1GO.transform.localScale.x * 0.5f;
+        rect1.position = rect1GO.transform.position;
+        rect1.extents = new Vector2(rect1GO.transform.localScale.x, rect1GO.transform.localScale.y) * 0.5f;
 
         //Color color = CheckCollisionCircles(position1, radius1, position2, radius2) ? Color.green : Color.red;
         //circle1.GetComponent<SpriteRenderer>().color = color;
         //circle2.GetComponent<SpriteRenderer>().color = color;
 
-        Color color = CheckCollisionCircleRect(position1, radius1, rectPosition, rectExtents) ? Color.green : Color.red;
-        circle1.GetComponent<SpriteRenderer>().color = color;
-        rect1.GetComponent<SpriteRenderer>().color = color;
+        //Color color = CheckCollisionCircleRect(position1, radius1, rectPosition, rectExtents) ? Color.green : Color.red;
+        Color color = CheckCollisionCircleRect(circle1, rect1) ? Color.green : Color.red;
+        circle1GO.GetComponent<SpriteRenderer>().color = color;
+        rect1GO.GetComponent<SpriteRenderer>().color = color;
     }
 
     bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2)
@@ -57,5 +77,26 @@ public class Detection : MonoBehaviour
         else if (test.y >= yMax) test.y = yMax;
 
         return Vector2.Distance(test, circle) <= radius;
+    }
+
+    bool CheckCollisionCircleRect(Circle circle, Rectangle rectangle)
+    {
+        // If its annoying to type things out in full like rectangle.position and rectangle.extents,
+        // you can make a short-hand variable that refers to said attributes!
+        // ***IF SHORT-HAND VARIABLES ARE STRUCTS, THEY WILL BE COPIED!!!***
+        Vector2 rp = rectangle.position;
+        Vector2 re = rectangle.extents;
+        float xMin = rp.x - re.x;
+        float xMax = rp.x + re.x;
+        float yMin = rp.y - re.y;
+        float yMax = rp.y + re.y;
+
+        Vector2 test = circle.position;
+        if (test.x <= xMin) test.x = xMin;
+        else if (test.x >= xMax) test.x = xMax;
+        if (test.y <= yMin) test.y = yMin;
+        else if (test.y >= yMax) test.y = yMax;
+
+        return Vector2.Distance(test, circle.position) <= circle.radius;
     }
 }
