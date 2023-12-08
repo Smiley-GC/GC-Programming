dc550a0becc3a34f901dcf163eb0a7bb9a138309
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-08 10:50:22 -0500

    Made fireworks

--- Assets/Scripts/Math/PhysicsSystem.cs
+++ Assets/Scripts/Math/PhysicsSystem.cs
@@ -8,48 +8,53 @@ public class Particle
     public Vector2 pos;
     public Vector2 vel;
     public Vector2 acc;
+    public float age;
 }
 
 public class PhysicsSystem : MonoBehaviour
 {
     public GameObject prefab;
-
-    // Single object
-    //Particle particle = new Particle();
-    //GameObject particleGO;
-    
-    // Multiple objects
     List<Particle> particles = new List<Particle>();    // Physics
     List<GameObject> objects = new List<GameObject>();  // Rendering
+
+    float currentTime = 0.0f;
+    float totalTime = 1.0f;
 
     void Add(Particle particle)
     {
         particles.Add(particle);
-        objects.Add(Instantiate(prefab));
+
+        float r = Random.Range(0.0f, 1.0f);
+        float g = Random.Range(0.0f, 1.0f);
+        float b = Random.Range(0.0f, 1.0f);
+        GameObject obj = Instantiate(prefab);
+        obj.GetComponent<SpriteRenderer>().color = new Color(r, g, b);
+        objects.Add(obj);
     }
 
-    private void ResetParticles()
+    void Remove(Particle particle)
     {
-        for (int i = 0; i < 10; i++)
+        int index = particles.IndexOf(particle);
+        particles.RemoveAt(index);
+        Destroy(objects[index]);
+        objects.RemoveAt(index);
+    }
+
+    void CreateBatch(Vector2 position, int count)
+    {
+        for (int i = 0; i < count; i++)
         {
-            Particle particle = particles[i];
-            particle.pos = Vector2.zero;
+            Particle particle = new Particle();
+            particle.pos = position;
             particle.vel = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
             particle.acc = Physics.gravity;
+            Add(particle);
         }
     }
 
     void Start()
     {
-        // Single object
-        //particleGO = Instantiate(prefab);
-        //particle.vel = new Vector2(0.0f, 10.0f);
-        //particle.acc = Physics.gravity;
-
-        // Multiple objects
-        for (int i = 0; i < 10; i++)
-            Add(new Particle());
-        ResetParticles();
+        CreateBatch(Vector2.zero, 16);
     }
 
     // pi = 5m, v = 2.5ms, t = 2s
@@ -63,7 +68,6 @@ public class PhysicsSystem : MonoBehaviour
     void FixedUpdate()
     {
         float dt = Time.fixedDeltaTime;
-
         for (int i = 0; i < particles.Count; i++)
         {
             Particle particle = particles[i];
@@ -73,17 +77,29 @@ public class PhysicsSystem : MonoBehaviour
 
             // 2. Add velocity * time to position
             particle.pos = Integrate(particle.pos, particle.vel, dt);
-
-            // 3. Write position from particle to game object
-            objects[i].transform.position = particle.pos;
         }
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        float dt = Time.deltaTime;
+        for (int i = 0; i < particles.Count; i++)
         {
-            ResetParticles();
+            Particle particle = particles[i];
+            objects[i].transform.position = particle.pos;
+
+            particle.age += dt;
+            if (particle.age > 2.5f)
+                Remove(particle);
+        }
+
+        currentTime += dt;
+        if (currentTime > totalTime)
+        {
+            currentTime = 0.0f;
+            CreateBatch(new Vector2(Random.Range(-20.0f, 20.0f), Random.Range(-10.0f, 10.0f)), 16);
+            totalTime += Random.Range(-0.5f, 0.5f);
+            totalTime = Mathf.Max(0.25f, totalTime);
         }
     }
 }
